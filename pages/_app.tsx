b65fd9200b8323a1318747eafeaa2e16dae2b6d4
iamminci <0xminci@gmail.com>
2022-09-17 02:31:01 +0200

    ADDED: chakra UI and fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: (props: any) => ({
+      body: {
+        fontFamily: "Bai Jamjuree",
+        lineHeight: "base",
+      },
+    }),
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
